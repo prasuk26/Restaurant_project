e1fe133783f4dbde7096a9851edcb3b1221059c4
Prasuk <prasukjain2607@gmail.com>
2023-06-28 23:43:44 +0530

    find us completed

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
-
+import {images} from '../../constants';
+import { SubHeading } from '../../components';
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className='app__bg app__wrapper section__padding ' id='contact'>
+    <div className="app__wrapper_info">
+      <SubHeading title="Contact"/>
+      <h1 className="headtext__cormorant" style={{marginBottom: '3rem'}}>Find Us</h1>
+      <div className="app_wrapper-content">
+        <p className='p__opensans'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa ipsum </p>
+        <p className="p__cormorant" style={{color: "#DCCA87" , margin: '2rem 0'}}Opening Hours></p>
+        <p className="p__opensans">Mon-Fri: 10:00am - 03:00am</p>
+        <p className="p__opensans">Sat-Sun: 10:00am - 02:00am</p>
+      </div>
+      <button className='custom__button' style={{marginTop: '2rem'}}>Visit Us</button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.findus} alt="" />
+    </div>
   </div>
 );
 
